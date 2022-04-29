752f8cd0bc69bd9596a2d4e42295580fd45c173a
Justin2172021 <justin.busching@gmail.com>
2022-04-29 16:09:51 -0500

    Added module function for array rendering to screen/game board

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Setting up the gameBoard module
 let gameBoardModule = (function() {
-    let gameBoard = [];
-    return {};
+    let gameBoard = ["X"];
+    return {gameBoard};
 })();
 
-// Setting up the displayController module
+// Setting up the displayController module to control who's turn it is
 let displayControllerModule = (function() {
     let testF = () => {console.log("testing private function call inside of a module object.....")};
     return {testF};
@@ -17,8 +17,20 @@ let createPlayer = (playerName, playerNumber, assignedXO) => {
     return {getPlayerName, playerName, playerNumber, assignedXO};
 };
 
+// Setting up the render module for array display to gameboard
+let renderArrayToScreenModule = (function() {
+    
+    // TODO: Refactor this (dont need to query select a gridbox - target from click will take care of that) once I have the click event module setup 
+    const gridBoxes = document.querySelectorAll(".grid-box");
+    gridBoxes[0].textContent = gameBoardModule.gameBoard;
+    console.log("show me the array as seen inside of renderArrayToScreenModule...", gameBoardModule.gameBoard)
+    console.log("show me the node list of the gridBoxes as seen inside of renderArrayToScreenModule...", gridBoxes);
+    return {};
+})();
+
 // TODO: Create inputs for name, and assignedXO - asking them for their preference (maybe a pop up message box?)
 // TODO: Do I need to assign player numbers?  Might be unneccesary.  Something to check later.
 
 let Justin = createPlayer("Justin", 1, "X");
 let James = createPlayer("James", 2, "O");
+renderArrayToScreenModule;
